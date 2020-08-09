540b9bd08ec12832701c2b58d227ee59f970de84
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-08-09 13:06:14 +0000

    done

---
+++ kata_7.js
@@ -0,0 +1,25 @@
+const checkAir = function (samples, threshold) {
+  const totalSamples = samples.length;
+  let totalDirtySamples = 0;
+  for (let sample of samples) {
+    if (sample === 'dirty') {
+      totalDirtySamples++;
+    }
+  }
+  return totalDirtySamples/totalSamples < threshold ? 'Clean' : 'Polluted';
+};
+
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+))
