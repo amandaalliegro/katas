11befb442096bf28b8d4d222baa87ab93f7af0bd
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-08-09 02:07:18 +0000

    done

---
+++ kata_3.js
@@ -0,0 +1,13 @@
+const numberOfVowels = function(data) {
+  let sum = 0;
+  for (let char of data) {
+    if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+      sum++;
+    }
+  }
+  return sum;
+};
+
+console.log(numberOfVowels("orange"));
+console.log(numberOfVowels("lighthouse labs"));
+console.log(numberOfVowels("aeiou"));
